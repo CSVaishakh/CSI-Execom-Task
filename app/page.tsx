3da95ec11a713d054591e17437e41be874f14e38
C S Vaishakh <vaishakhsubramonian@gmail.com>
2025-08-23 22:48:22 +0530

    feat: add LandingHero and SBDetails components to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
 import { LandingHeader } from "@/components/headers";
+import LandingHero from "@/components/landingHero";
+import SBDetails from "@/components/sbDetails";
 
 export default function Home() {
   return (
     <section>
       <LandingHeader/>
+      <LandingHero/>
+      <SBDetails/>
     </section>
   );
 }
